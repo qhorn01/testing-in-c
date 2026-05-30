fed7c035f905b4a497db76a2af2e992401abcd8a
qhorn01 <quentinediting@gmail.com>
2026-05-30 17:27:59 -0400

    Added a black square to screen

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,20 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
+const unsigned char blackTile[] = {
+    0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF,
+    0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF
+};
+
 void main() {
     printf("Hello world");
+
+
+    set_bkg_data(1, 1, blackTile);
+
+    set_bkg_tile_xy(10, 2, 1);
+
+    SHOW_BKG;
 
     while(1) {
         vsync(); 
